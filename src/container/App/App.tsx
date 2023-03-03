984d6cdc29daa08a94a335e95b404e3b621d99b0
Angelina <angelina@MacBook-Air-Angelina.local>
2023-03-03 20:32:13 +0200

    connect react-router-dom

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,20 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
-import Main from 'container/Main/Main'
+import Home from 'pages/Home/Home'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 
 type Props = {}
 
 const App = (props: Props) => {
-    return (
-        <StyledEngineProvider injectFirst>
-            <CssBaseline />
-            <Header />
-            <Main />
-            <Footer />
-        </StyledEngineProvider>
-    )
+  return (
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </StyledEngineProvider>
+  )
 }
 
 export default App
